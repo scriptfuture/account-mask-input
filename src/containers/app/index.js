27ae9112e506fed95303459b47462d9a5472d72b
scriptfuture <progratronic@gmail.com>
2019-02-25 02:49:21 +0300

    start commit

---
+++ src/containers/app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import Home from '../home'
+import About from '../about'
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Главная</Link>&nbsp; 
+      <Link to="/about-us">О сайте</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
+
+export default App
